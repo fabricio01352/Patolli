7dd46c6074cdc95c0423dd450e975c2a4b7aa967
Deibi <121470260+deiviLedesma@users.noreply.github.com>
2024-12-03 18:27:48 -0700

    Add files via upload

---
+++ PatolliLogica/src/main/java/com/mycompany/patollilogica/PatolliLogica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.patollilogica;
+
+/**
+ *
+ * @author SDavidLedesma
+ */
+public class PatolliLogica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
